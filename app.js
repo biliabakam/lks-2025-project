be1a73fdb14e52be3759a2da74833f7c8fa92465
biliabakam <biliabakam@gmail.com>
2025-06-18 15:26:15 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const OpenAI = require('openai');
+const axios = require('axios');
+const app = express();
+
+app.use(express.json());
+
+app.get();
+
+app.post('/ask', async (req, res) => {
+    const { prompt } = req.body;
+    try {
+        const response = await axios.post('http://llm-project) 
+    }}
+);
+
---
+++ scripts/start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd C:/Users/Lenovo/llm-project/scripts/start_server.sh
+npm install
+npm 
