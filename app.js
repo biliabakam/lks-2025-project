80b8ebf60a982df120fc320af08e4a5c47488111
biliabakam <biliabakam@gmail.com>
2025-06-18 15:35:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.get();
 app.post('/ask', async (req, res) => {
     const { prompt } = req.body;
     try {
-        const response = await axios.post('http://llm-project) 
-    }}
-);
+        const response = await axios.post('http://llm-project:5000/generate', { prompt });
+        res.json(response.data);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
 
+app.listen(3000, () => console.log('API running on port 3000'));
